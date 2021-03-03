cc865593acee5da5b71065de6fc2b2b36bd9f71d
wjq2002 <271736888@qq.com>
2021-03-03 17:25:27 +0800

    third

--- CSC1002_study/2021_3_3/1.py
+++ CSC1002_study/2021_3_3/1.py
@@ -1,11 +1,13 @@
+import string
 m = 'SPY CODER'
 
 chr(ord('A')+2)
 
 chr(65)
 
-#Shift one letter
+#Shift one letter,Uppercase only
 def shift(letter, value):
+    return shift3(letter, value)
     if letter.isupper():
         ch = chr(ord(letter)+value)
         if(ch>'Z'):
@@ -14,8 +16,10 @@ def shift(letter, value):
             ch = chr(ord(letter)+value+26)
         return ch
     return letter
-    
+
+
 def shift2(letter,value):
+    
     if letter.isalpha():
         ch = chr(ord(letter.upper())+value)
         if(ch>'Z'):
@@ -26,16 +30,23 @@ def shift2(letter,value):
         return ch if letter.isupper() else ch.lower()
     
     return letter
-    
+
+#Custom only, but in one case only
+def shift3(letter, value, alpha = string.ascii_uppercase):
+    if letter in alpha:
+        idx = (alpha.find(letter) + value) % len(alpha)
+        return alpha[idx]
+    return letter
+
 #Encode the message(m) using caesars cipher
 def shift_cipher(m, value):
     secret = ''
     for c in m:
-        secret += shift2(c, value)
+        secret += shift3(c, value, 'ABC')
     return secret
-    
+
 #m = 'S'
-m = 'Spy Coder'
+m = 'SPY CODER'
 ans = shift_cipher(m,5)
 print(ans)
 original = shift_cipher(ans, -5)
