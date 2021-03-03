cbc3d35d1f112c7d070cf06b8011759aa5b8e6ff
wjq2002 <271736888@qq.com>
2021-03-03 17:04:40 +0800

    first

---
+++ CSC1002_study/2021_3_3/1.py
@@ -0,0 +1,47 @@
+m = 'SPY CODER'
+
+chr(ord('A')+2)
+
+chr(65)
+
+#Shift one letter
+def shift(letter, value):
+    if letter.isupper():
+        ch = chr(ord(letter)+value)
+        if(ch>'Z'):
+            ch = chr(ord(letter)+value-26)
+        if(ch<'A'):
+            ch = chr(ord(letter)+value+26)
+        return ch
+    return letter
+    
+def shift2(letter,value):
+    if letter.isupper():
+        ch = chr(ord(letter)+value)
+        if(ch>'Z'):
+            ch = chr(ord(letter)+value-26)
+        if(ch<'A'):
+            ch = chr(ord(letter)+value+26)
+        return ch
+    elif letter.islower():
+        ch = chr(ord(letter)+value)
+        if(ch>'z'):
+            ch = chr(ord(letter)+value-26)
+        if(ch<'a'):
+            ch = chr(ord(letter)+value+26)
+        return ch
+    
+    return letter
+#Encode the message(m) using caesars cipher
+def shift_cipher(m, value):
+    secret = ''
+    for c in m:
+        secret += shift2(c, value)
+    return secret
+    
+#m = 'S'
+m = 'Spy Coder'
+ans = shift_cipher(m,5)
+print(ans)
+original = shift_cipher(ans, -5)
+print(original)
