442b65f3256f30a2ecfbedc9cf2e8f7ca2b8e52f
wjq2002 <271736888@qq.com>
2021-03-08 11:57:29 +0800

    2021_3_8

--- Document/class_study.py
+++ Document/class_study.py
@@ -332,7 +332,7 @@ print(fun__())
 
 from math import sin
 
-fun = input()
+fun = sin
 x = int(input())
 exec(f'y = {fun}(x)')
 print(y)
@@ -341,4 +341,27 @@ print(y)
 ord('S')
 chr(65)
 #ASCII 码与字符转换
+
+handle = open('D:\python_work/Document/test.txt','r')
+# alltext = handle.read()
+# print(len(alltext))
+# print(alltext[:20])
+
+# for line in handle:
+#     print(line,end = '')
+for line in handle:
+    if line.startswith('2'):                    #判断行开头是不是'2'
+        print(line,end = '') 
+handle.close()
+
+handle = open('D:\python_work/Document/test.txt','w')
+handle.write('233\n666 666\n999 999 999')       #write的内容只能是string
+
+a = '1234'
+#a[1] = '3'                  #! str不能改变元素的值
+a = list(a)
+a[1] = '3'
+b = ''.join(a)               #join只能用于字符，不能用于数字和列表
+print(b)                     #先将字符串转成列表，再修改列表值，再join为字符串
+
 
---
+++ Document/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+for line in handle:
+#     print(line,end = '')
